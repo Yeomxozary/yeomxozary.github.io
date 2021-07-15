95014e9246fd046052468d28385f45c09af16c38
Yeomxozary <imfalih@gmail.com>
2021-07-15 21:10:36 +0700

    Nismo Dicoding 1 by Mail
    Basic Web Programming Submission at Dicoding, only 1 page about Nissan especially Nismo.

---
+++ Nismo Dicoding 1 by Mail/script.js
@@ -0,0 +1,34 @@
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slideshow");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" dot-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " dot-active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
